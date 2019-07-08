bea6d7f887e17e171de062ea2e74da4939461d2b
mrpyrex <ndifrkeumoren@gmail.com>
2019-07-08 21:40:31 +0100

    Adjusted api views

--- shop/api.py
+++ shop/api.py
@@ -1,21 +1,30 @@
 from shop.models import Product, Category
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework import viewsets, permissions
+from rest_framework import permissions
 from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView)
 
 
-class ProductAPIViewset(viewsets.ModelViewSet):
+class ProductAPIView(ListAPIView):
     queryset = Product.objects.all()
     permission_classes = [
         permissions.AllowAny
     ]
     parser_classes = (MultiPartParser, FormParser)
-
     serializer_class = ProductSerializer
     lookup_field = 'slug'
 
 
-class CategoryAPIViewset(viewsets.ModelViewSet):
+class ProductInCategoryView(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        return Product.objects.filter(category=slug)
+
+
+class CategoryAPIView(ListAPIView):
     queryset = Category.objects.all()
     permission_classes = [
         permissions.AllowAny
@@ -24,3 +33,25 @@ class CategoryAPIViewset(viewsets.ModelViewSet):
 
     serializer_class = CategorySerializer
     lookup_field = 'slug'
+
+
+# class ProductAPIViewset(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+#     permission_classes = [
+#         permissions.AllowAny
+#     ]
+#     parser_classes = (MultiPartParser, FormParser)
+
+#     serializer_class = ProductSerializer
+#     lookup_field = 'slug'
+
+
+# class CategoryAPIViewset(viewsets.ModelViewSet):
+#     queryset = Category.objects.all()
+#     permission_classes = [
+#         permissions.AllowAny
+#     ]
+#     parser_classes = (MultiPartParser, FormParser)
+
+#     serializer_class = CategorySerializer
+#     lookup_field = 'slug'
--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,17 @@
-from rest_framework.routers import DefaultRouter
-from .api import ProductAPIViewset, CategoryAPIViewset
+from django.urls import path
+from .api import ProductAPIView, CategoryAPIView, ProductInCategoryView
 
-router = DefaultRouter()
-router.register('', ProductAPIViewset, basename='shop')
-urlpatterns = router.urls
+
+urlpatterns = [
+
+    path('', ProductAPIView.as_view()),
+    path('categories/', CategoryAPIView.as_view()),
+    path('category-filter/<slug>', ProductInCategoryView.as_view())
+]
+
+
+# from rest_framework.routers import DefaultRouter
+# ProductAPIViewset, CategoryAPIViewset
+# router = DefaultRouter()
+# router.register('', ProductAPIViewset, basename='shop')
+# urlpatterns = router.urls
