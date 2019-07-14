cc314344c55e6006f3a3be0002704f22f692794c
mrpyrex <ndifrkeumoren@gmail.com>
2019-07-14 18:52:18 +0100

    Added twitter feed

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Jumbotron from "../../layout/Jumbotron";
 import Subscribe from "../../layout/Subscribe";
+import Reviews from "../../layout/Reviews";
 
 const Home = () => {
   return (
     <div>
       <Jumbotron />
       <Subscribe />
+      <Reviews />
     </div>
   );
 };
--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,18 +1,50 @@
 import React from "react";
+import Subscribe from "./Subscribe";
 
 const Footer = () => {
   return (
     <footer>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <h5>Address</h5>
+          <div className="col-md-4 ">
+            <div className="row">
+              <div className="col-sm-12">
+                <p>14, Ring Road Idua, Eket, Akwa Ibom State</p>
+                <p>
+                  <i class="fas fa-phone" /> +234 7080 230 375
+                </p>
+                <p>
+                  <i class="fas fa-envelope-open-text" />{" "}
+                  info@jasminecakesandchops.com
+                </p>
+                <p>
+                  <i class="fab fa-facebook-square" />
+                  <i class="fab fa-instagram" />
+                  <i class="fab fa-twitter" />
+                  <i class="fab fa-pinterest-p" />
+                </p>
+              </div>
+            </div>
           </div>
           <div className="col-md-4">
-            <h5>Subscribe</h5>
+            <div className="row">
+              <div className="col-sm-12">
+                <Subscribe />
+              </div>
+            </div>
           </div>
           <div className="col-md-4">
-            <h5>Twitter feed</h5>
+            <a
+              class="twitter-timeline"
+              data-width="400"
+              data-height="400"
+              data-theme="dark"
+              data-link-color="#E81C4F"
+              href="https://twitter.com/neupytech?ref_src=twsrc%5Etfw"
+              // data-tweet-limit="5"
+            >
+              Tweets by neupytech
+            </a>
           </div>
         </div>
       </div>
